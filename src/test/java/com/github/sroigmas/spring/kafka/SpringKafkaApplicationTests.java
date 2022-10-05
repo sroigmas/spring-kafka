095a738cc7b957cf72c6d052b5dd50b4976c3e1e
Salva Roig <salroima@gmail.com>
2022-10-06 01:34:46 +0200

    Initial commit

---
+++ src/test/java/com/github/sroigmas/spring/kafka/SpringKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sroigmas.spring.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
