543c8e512a70506a3925c814121b59d23b117f49
Salva Roig <salroima@gmail.com>
2022-10-07 13:30:26 +0200

    Creating topic programmatically (TopicBuilder)

--- src/main/java/com/github/sroigmas/spring/kafka/config/KafkaConfiguration.java
+++ src/main/java/com/github/sroigmas/spring/kafka/config/KafkaConfiguration.java
@@ -1,6 +1,9 @@
 package com.github.sroigmas.spring.kafka.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaConfiguration {
@@ -37,4 +40,21 @@ public class KafkaConfiguration {
   public ConsumerFactory<String, String> consumerFactory() {
     return new DefaultKafkaConsumerFactory<>(consumerProperties());
   }*/
+
+  /*@Bean
+  public KafkaAdmin kafkaAdmin() {
+    return new KafkaAdmin(Map.of(
+        AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"
+    ));
+  }*/
+
+  @Bean
+  public NewTopic hobbit2() {
+    return TopicBuilder.name("hobbit2")
+        .partitions(10)
+        .replicas(3)
+        //.compact()
+        //.config(TopicConfig.COMPRESSION_TYPE_CONFIG, "zstd")
+        .build();
+  }
 }
